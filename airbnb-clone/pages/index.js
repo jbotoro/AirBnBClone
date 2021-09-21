ae46d8119840ad0e81323c010dcbc4212078b994
Jordan Black <jblack530@gmail.com>
2021-09-21 15:06:59 -0700

    added tailwind-scrollbar-hide built out smallcard and mediumcard components, applied static rendering for small card and medium card, created main and 2 sections in index.js

--- airbnb-clone/pages/index.js
+++ airbnb-clone/pages/index.js
@@ -1,18 +1,66 @@
 import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import MediumCard from '../components/MediumCard';
+import SmallCard from '../components/SmallCard';
 
-export default function Home() {
-  return (
-    <div className="">
-      <Head>
-        <title>JBBnB </title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      
-      <Header />
-      <Banner />
-      { /*  */ }
-    </div>
-  )
-}
+export default function Home({exploreData, cardsData}) {
+    return (
+      <div className="">
+        <Head>
+          <title>JBBnB </title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        
+        <Header />
+        <Banner />
+        { /* Main */ }
+
+        <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+          <section className='pt-6'>
+            <h2 className='text-4xl font-semibold pb-5'> Explore Nearby </h2>
+
+            {/* Pull data from server - API endpoints */}
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+              {exploreData?.map(({img, location, distance}) => (
+                <SmallCard 
+                  key={img}
+                  img={img} 
+                  distance={distance} 
+                  location={location}
+                />
+              ))}
+            </div>
+          </section>
+
+          <section>
+            <h2 className='text-4xl font-semibold py-8'>Live Anywhere </h2>
+            <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3'>
+              {cardsData.map(({img,title}) => (
+                <MediumCard key={img} img={img} title={title} />
+              ))}
+            </div>
+          </section>
+        </main>
+      </div>
+    )
+  }
+
+  export async function getStaticProps() {
+    const exploreData = await fetch('https://links.papareact.com/pyp').
+    then(
+      (res) => res.json()
+    );
+
+    const cardsData = await fetch('https://links.papareact.com/zp1').
+    then(
+      (res) => res.json()
+    )
+
+    return {
+      props: {
+        exploreData,
+        cardsData
+      }
+    }
+  }
