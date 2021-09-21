c72a2d065a9eb77eda3401543ce629ee5243afbf
Jordan Black <jblack530@gmail.com>
2021-09-21 11:01:09 -0700

    started airbnb clone with create-next-app with tailwind config, added first two components to Home page: Header and Banner, started building out Header

---
+++ airbnb-clone/pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>JBBnB </title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      < Header />
+      { /* Banner */ }
+      { /*  */ }
+    </div>
+  )
+}
