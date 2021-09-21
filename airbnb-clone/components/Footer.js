830397963d3a7dbc0a8959cd301667eda786f337
Jordan Black <jblack530@gmail.com>
2021-09-21 15:44:53 -0700

    finished large card component, as well as footer component, near finished front end day 1 complete

---
+++ airbnb-clone/components/Footer.js
@@ -0,0 +1,40 @@
+function Footer() {
+    return (
+        <div className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
+            <div className='space-y-4 text-xs text-gray-800'>
+                <h5 className='font-bold'> ABOUT </h5>
+                <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+            </div>
+            <div className='space-y-4 text-xs text-gray-800'>
+                <h5 className='font-bold'> COMMUNITY </h5>
+                <p>Accessibility</p>
+                <p>This is not a real site</p>
+                <p>It's just a clone, bro</p>
+                <p>Referrals accepted</p>
+                <p>Jordan B</p>
+            </div>
+            <div className='space-y-4 text-xs text-gray-800'>
+                <h5 className='font-bold'> HOST </h5>
+                <p>Jordan Black</p>
+                <p>Presents</p>
+                <p>JBBnB</p>
+                <p>React</p>
+                <p>Next.js</p>
+            </div>
+            <div className='space-y-4 text-xs text-gray-800'>
+                <h5 className='font-bold'> SUPPORT </h5>
+                <p>Help Center</p>
+                <p>Trust & Safety</p>
+                <p>Moar Text</p>
+                <p>Easter Egg</p>
+                <p>FTW!</p>
+            </div>      
+        </div>
+    )
+}
+
+export default Footer
